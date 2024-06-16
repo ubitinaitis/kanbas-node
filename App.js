fce5bbd51ccb1e8cfd1fb793caf3052d986bc6e7
ubitinaitis <bitinaitis.u@northeastern.edu>
2024-06-16 01:51:18 -0400

    mongo

--- App.js
+++ App.js
@@ -1,17 +1,24 @@
 import express from 'express'
+import mongoose from "mongoose";
 import Hello from "./Hello.js"
+import "dotenv/config";
 import Lab5 from "./Lab5/index.js";
 import CourseRoutes from "./Kanbas/Courses/routes.js";
 import ModuleRoutes from "./Kanbas/Modules/routes.js";
 import AssignmentRoutes from "./Kanbas/Assignments/routes.js";
 import cors from "cors";
+import UserRoutes from "./Kanbas/Users/routes.js";
 
+const CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas"
+mongoose.connect(CONNECTION_STRING);
 const app = express();
 app.use(cors());    
 app.use(express.json());
+UserRoutes(app);
 AssignmentRoutes(app);
 ModuleRoutes(app);
 CourseRoutes(app);
 Lab5(app)
 Hello(app)
 app.listen(process.env.PORT || 4000)
+
